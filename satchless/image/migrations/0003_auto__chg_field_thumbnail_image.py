fefc236b4383e8c013c86005161b1ba51228856e
Chris Proto <cproto@fusionbox.com>
2012-01-18 14:07:36 -0700

    thumbnail image migration

---
+++ satchless/image/migrations/0003_auto__chg_field_thumbnail_image.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Changing field 'Thumbnail.image'
+        db.alter_column('image_thumbnail', 'image', self.gf('django.db.models.fields.files.ImageField')(max_length=255))
+
+
+    def backwards(self, orm):
+        
+        # Changing field 'Thumbnail.image'
+        db.alter_column('image_thumbnail', 'image', self.gf('django.db.models.fields.files.ImageField')(max_length=100))
+
+
+    models = {
+        'image.image': {
+            'Meta': {'object_name': 'Image'},
+            'height': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'width': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        'image.thumbnail': {
+            'Meta': {'unique_together': "(('image', 'size'),)", 'object_name': 'Thumbnail'},
+            'height': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
+            'original': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['image.Image']"}),
+            'size': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'width': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['image']
